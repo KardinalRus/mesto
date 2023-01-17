0529ef786cb5dd11ca1cd7fb94b5464d4f93819b
Ruslan Safin <kardinal77707@yandex.ru>
2023-01-17 21:29:55 +0700

    исправлены замечания ревьювера

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,20 @@ let editBtnClick = document.querySelector('.profile__edit-btn');
 let popup = document.querySelector('.popup');
 let closeBtnClick = document.querySelector('.popup__close');
 let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__description');
+let nameInput = formElement.querySelector('.popup__box_type_name');
+let jobInput = formElement.querySelector('.popup__box_type_description');
 let userName = document.querySelector('.profile__name');
 let userJob = document.querySelector('.profile__description');
 
 
 function openPopup() {
-  popup.classList.add('popup__opened');
+  popup.classList.add('popup_opened');
   nameInput.value = userName.textContent;
   jobInput.value = userJob.textContent;
 };
 
 function closePopup() {
-  popup.classList.remove('popup__opened');
+  popup.classList.remove('popup_opened');
 };
 
 function handleFormSubmit (evt) {
